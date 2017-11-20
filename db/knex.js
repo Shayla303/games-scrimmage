e29dee45a9efedee3eed58def31ea13ec39682ba
Shayla Richard <shaylarichard@hotmail.com>
2017-11-20 11:54:46 -0700

    third deploy'

--- db/knex.js
+++ db/knex.js
@@ -1,5 +1,5 @@
 const knex = require('knex')
-const environment = 'development'
+const environment = process.env.NODE_ENV ||'development'
 const config = require('../knexfile.js')
 const db = knex(config[environment])
 
